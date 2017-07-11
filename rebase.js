1c8b7ba9a8887df8227f9aae720f3900e02c6ce9
taylor <taylor.wan@fiscalnote.com>
2017-07-11 12:40:57 -0400

    xenophobia?

--- rebase.js
+++ rebase.js
@@ -1,2 +1,3 @@
 console.log('Testing rebase and merge...');
 // TODO: what if i just add more stuff here one day?
+// TODO: hoping project r really works but am still kind of scared of them reading our repo
